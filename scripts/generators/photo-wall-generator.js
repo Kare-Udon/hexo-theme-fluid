a7e68f3e1e8caeb0e7167d0b397aa5efaa56f84a
Kare-Udon <laoliu735@gmail.com>
2025-11-19 11:15:26 +0800

    fix: 照片墙月份问题

--- scripts/generators/photo-wall-generator.js
+++ scripts/generators/photo-wall-generator.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 module.exports =  function (locals) {
   const config = this.config;
-  
+
   const photoDir = path.join(config.source_dir, 'images/photo_wall');
+  const ymMap = new Map();
   var photoData = {
     yms: [],
-    data: []
+    data: [],
+    collections: []
   };
 
   fs.readdirSync(photoDir).forEach(year => {
@@ -20,28 +22,34 @@ module.exports =  function (locals) {
             const photoPath = path.join('/images/photo_wall', year, month, photo);
             if (!photoData.yms.find(ym => ym.year === year && ym.month === month)) {
               photoData.yms.push({year, month});
-            };
-            photoData.data.push({
-              year,
-              month,
-              photo: photoPath
-            });
+            }
+
+            const ymKey = `${year}-${month}`;
+            if (!ymMap.has(ymKey)) {
+              ymMap.set(ymKey, []);
+            }
+            const info = { year, month, photo: photoPath };
+            ymMap.get(ymKey).push(info);
+            photoData.data.push(info);
           });
         }
       });
     }
   });
 
-  photoData.yms.sort((a, b) => {
-    const dateA = new Date(a.year, a.month - 1);
-    const dateB = new Date(b.year, b.month - 1);
+  const sortByDateDesc = (arr) => arr.sort((a, b) => {
+    const dateA = new Date(Number(a.year), Number(a.month) - 1);
+    const dateB = new Date(Number(b.year), Number(b.month) - 1);
     return dateB - dateA;
   });
 
-  photoData.data.sort((a, b) => {
-    const dateA = new Date(a.year, a.month - 1);
-    const dateB = new Date(b.year, b.month - 1);
-    return dateB - dateA;
+  sortByDateDesc(photoData.yms);
+  sortByDateDesc(photoData.data);
+
+  photoData.collections = photoData.yms.map(({ year, month }) => {
+    const ymKey = `${year}-${month}`;
+    const photos = sortByDateDesc(ymMap.get(ymKey) || []);
+    return { year, month, photos };
   });
 
   return {
